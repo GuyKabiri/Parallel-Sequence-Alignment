7a6de3806aba4166d5bfcd55916a23bcf462dbe7
Guy <guyk1994@gmail.com>
2021-07-10 16:33:44 +0300

    Add comments and move functions from main.c to cpu.c

--- main.h
+++ main.h
@@ -5,8 +5,5 @@
 #include "cpu_funcs.h"
 
 int main(int argc, char* argv[]);
-int find_mutant(char* seq1, char* seq2, double* weights, int offset, char* mutant, int is_max);
-char maximize(char c1, char c2, double* weights);
-char minimize(char c1, char c2, double* weights);
 
 #endif //__MAIN_H__
